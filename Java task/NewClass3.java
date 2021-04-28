2af5297779ee61aa2512b16e528fdaddf306a719
MahmoudKhider <81014558+MahmoudKhider@users.noreply.github.com>
2021-04-28 17:46:10 +0200

    Add files via upload

---
+++ Java task/NewClass3.java
@@ -0,0 +1,26 @@
+
+package task;
+
+import static java.lang.Math.*;         //* لكل الي موجود في Math
+//import static java.lang.Math.ceil;
+
+/*
+Built -in funchions - methods
+Java number method
+Java String method
+ava Array method
+*/
+
+public class NewClass3 {
+    public static void main(String[]args){
+        
+        System.out.println("abs="+abs(-1.7));  // Static double abs(double r)
+        System.out.println("ceil="+ceil(-3.5));   //بياخد اكبر عدد صحيح
+        System.out.println("floor="+floor(1.9));//لصغر عدد صحيح
+        System.out.println("sqrt="+sqrt(100)); //الجزر التربيعي للعدد
+        System.out.println("power="+pow(2,3));//دي معناها (3اس2)
+        System.out.println("random="+random()); //0.0<= val < 1.0
+        
+    }
+    
+}
